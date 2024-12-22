97a18ddce8595868b099f1708992995141b50a6b
nawsish <nawsish.ahmed@spectrum-bd.com>
2024-12-22 12:31:22 +0600

    program file

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,13 @@
 using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlite("Data Source=auth.db"));
+builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
