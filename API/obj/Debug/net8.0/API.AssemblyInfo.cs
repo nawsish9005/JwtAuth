4915d94de59ad92a6311c81b934eefce4ad5e8c3
nawsish <nawsish.ahmed@spectrum-bd.com>
2024-12-23 15:44:57 +0600

    upload role

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f26c8f3938d06b5b61cbb084b64d0e617183379")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d70685712135b4ea80603f458ad4b69fdfb1716")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
