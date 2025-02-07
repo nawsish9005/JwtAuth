afead7f0e6782b1411d17dcd3f03ba14a20bcb0c
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-02-07 22:33:11 +0600

    add

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af5157392cd5066c7f9ecadbb367060877188522")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d670bd991f97afe840b7005a998aab711ddd73")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
